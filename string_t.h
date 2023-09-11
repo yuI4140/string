944677ae4e8e3dba61924a8cbd3b72d52e470efd
yuI4140 <greyberzambrano79@gmail.com>
2023-09-11 13:52:09 +0000

    update string_t

--- string_t.h
+++ string_t.h
@@ -21,43 +21,32 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-
+#include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
-#include <strings.h>
 typedef struct String {
   size_t size;
-  size_t cap;
   char value[];
 } String;
-typedef struct{
-    String **data;
-    size_t size;
-    size_t cap;
-}StringArray;
 void *ptrWrapper_s(void *ptr, int32_t line, const char *file);
 #define ptrWrapper(ptr) (ptrWrapper_s(ptr, __LINE__, __FILE__))
-// does a malloc but 'safe'
+// a memalloc but 'safe'
 void *allocPtr(size_t size);
-String *allocString(size_t size);
-String *newString(const char string[]);
-// do a string for a force move or copy to string->value
-String *implicitString(String *str);
-void appendString(String *str, const char string[]);
-// string to const char*
-const char *c_str(String *str);
-int stringCmp(String *str1, String *str2);
-void clearString(String *str);
-// get the specific char of a string with error handlig
-char atString(String *str, size_t index);
-void putcharString(String *str, char ch);
-void mvStrings(String *dest,String *src);
-void mvCharpToString(String *dest,const char*src);
-StringArray *newStringArray_s(size_t capacity,...);
-StringArray *concatToStringArray(StringArray *arr,char *str);
+#define STR_MALLOC allocPtr
+String *allocStr(size_t size);
+void clearStr(String *str);
+char atStr(String *str, size_t index);
+void mvStr(String *dest,String *src);
+void implicitStr(String *str);
+String *newStr(const char arr[]);
+// compare two sets of strings
+bool strEq(String *a, String *b);
+// is the same as strsub of C++
+void strCut(String *str, uint32_t start, uint32_t end);
+const char* c_str(String *str);
 #ifdef STRING_IMP
 void *ptrWrapper_s(void *ptr, int32_t line, const char *file) {
   if (ptr == NULL) {
@@ -70,122 +59,56 @@ void *allocPtr(size_t size) {
   void *ptr = malloc(size);
   return ptrWrapper(ptr);
 }
-String *allocString(size_t size) {
-  String *str = (String *)allocPtr(sizeof(String) + size);
+String *allocStr(size_t size) {
+  String *str = (String *)STR_MALLOC(sizeof(String) + size);
   str->size = size;
-  str->cap = size;
   return str;
 }
-String *newString(const char string[]) {
-  size_t length = strlen(string);
-  String *new = allocString(length);
-  for (size_t i = 0; i < length; ++i) {
-    new->value[i] = string[i]; 
-  }
-  return new;
-}
-String *implicitString(String *str){
-  size_t length = strlen(str->value);
-  String *imp=allocString(length);
-  return imp;
-}
-void appendString(String *str, const char string[]) {
-    size_t length = strlen(string);
-  if (str->size + length > str->cap) {
-    size_t new_cap = str->cap * 2;
-    while (str->size + length > new_cap) {
-      new_cap *= 2;
-    }
-    str = (String *)realloc(str, sizeof(String) + new_cap);
-    str->cap = new_cap;
-  }
-  for (size_t i = 0; i < length; ++i) {
-    str->value[str->size + i] = string[i];
-  }
-  str->size += length;
-}
-const char *c_str(String *str) {
-    str->value[str->size+1]='\0';
-    return str->value;
-}
-int stringCmp(String *str1, String *str2) {
-  size_t min_size = (str1->size < str2->size) ? str1->size : str2->size;
-  int cmp_result = strncmp(str1->value, str2->value, min_size);
-  if (cmp_result == 0) {
-    if (str1->size < str2->size) {
-      return -1;
-    } else if (str1->size > str2->size) {
-      return 1;
-    } else {
-      return 0;
-    }
-  }
-  return cmp_result;
-}
-void clearString(String *str) {
+void clearStr(String *str) {
     str->size=0;
-    str->cap=0; 
     memset(str->value, 0, str->size);
 }
-char atString(String *str, size_t index) {
+char atStr(String *str, size_t index) {
   if (index >= str->size) {
     fprintf(stderr, "Index out of bounds\n");
     exit(EXIT_FAILURE);
   }
   return str->value[index];
 }
-void putcharString(String *str, char ch) {
-    if (str->size + 1 > str->cap) {
-        size_t new_cap = str->cap * 2;
-        while (str->size + 1 > new_cap) {
-            new_cap *= 2;
-        }
-        str = (String *)realloc(str, sizeof(String) + new_cap);
-        str->cap = new_cap;
-    }
-    str->value[str->size] = ch;
-    str->size++;
-}
-void mvStrings(String *dest,String *src){
-    clearString(dest);
+void mvStr(String *dest,String *src){
+    clearStr(dest);
     dest->size=src->size; 
     memmove(dest->value,src->value,src->size);
-    dest->cap=src->cap;
+    dest->size=src->size;
 }
-void mvCharpToString(String *dest,const char*src){
-    clearString(dest);
+void mvCharpToStr(String *dest,const char*src){
+    clearStr(dest);
     dest->size=strlen(src); 
     memmove(dest->value,src,strlen(src)-1);
-    dest->cap=strlen(src)-1;
+    implicitStr(dest);
+} 
+void implicitStr(String *str){
+       size_t lengh=strlen(str->value);  
+       str->size=lengh;
 }
-StringArray *newStringArray_s(size_t capacity,...){
-    va_list args;
-    va_start(args,capacity);
-    StringArray *arr=allocPtr(sizeof(String));
-    arr->data=allocPtr(sizeof(String*));
-    size_t pos=0;
-    while (true) {
-        const char* arg=va_arg(args,const char*);
-        if (arg==NULL) {
-            break; 
-        }
-        String *item=newString(arg); 
-        ptrWrapper(item);
-        arr->size+=item->size; 
-        assert(arr->size!=arr->cap);
-        arr->data[pos++]=item;
-        ++arr->cap;
-    }  
-    va_end(args);
-    return ptrWrapper(arr);
+String *newStr(const char arr[]){
+    size_t size=strlen(arr);
+    String *str=allocStr(size);
+    memcpy(str->value,arr,size);
+    return str;
 }
-#define newStringArray(...) (newStringArray_s(1,__VA_ARGS__,NULL))
-StringArray *concatToStringArray(StringArray *arr,char *str){
+bool strEq(String *a, String *b){
+    return memcmp(a->value,b->value,a->size)!=0?false:true;
+}
+void strCut(String *str, uint32_t start, uint32_t end) {
     ptrWrapper(str);
-    ptrWrapper(arr);
-    String *new=newString(str);
-    arr->size+=new->size;
-    arr->data[arr->cap++]=new;
-    return ptrWrapper(arr);
+    assert(start < end && end <= str->size);
+    char *mv_str = str->value + start;
+    memmove(str->value, mv_str,end);
+    implicitStr(str);
 }
-#endif // STRING_IMP
+const char* c_str(String *str){
+   str->value[str->size] = '\0';  
+   return str->value;
+}
+#endif // END OF STRING_IMP
