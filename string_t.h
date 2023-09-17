c480e7cdd23c21b58eafc8f082fa203d956d9c9c
yuI4140 <greyberzambrano79@gmail.com>
2023-09-17 14:25:59 +0000

    add vstr_snprintf()

--- string_t.h
+++ string_t.h
@@ -56,6 +56,7 @@ String *floatToString(float num);
 #define fiterate_str(str) for(size_t it=0;it<str->size;++it)
 #define witerate_str(str) size_t it=0;while(str->value[it]!='\0')
 void str_snprintf(String *buffer,const char* fmt,...);
+void vstr_snprintf(String *buffer,const char* fmt,va_list args);
 #if defined(__STDC_VERSION__) && (__STDC_VERSION__ >= 201112L)
 #include <math.h>
 #include <errno.h>
@@ -197,6 +198,11 @@ String *floatToString(float num) {
 }
 void str_snprintf(String *buffer,const char* fmt,...){
     va_list args;
+    va_start(args,fmt);
+    (void)vsnprintf((char*)buffer->value,buffer->size,fmt,args);
+    va_end(args); 
+}
+void vstr_snprintf(String *buffer,const char* fmt,va_list args){
     va_start(args,fmt);
     (void)vsnprintf((char*)buffer->value,buffer->size,fmt,args);
     va_end(args); 
